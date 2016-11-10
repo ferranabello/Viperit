5dd4a3431d0e11edf2dc2d4dc4a216de209c6052
ferranabello <ferran.abello@gmail.com>
2016-11-10 23:18:40 +0100

    Xcode Template

---
+++ Xcode Templates/Viperit/Viperit Module.xctemplate/Universal/___FILEBASENAME___Presenter.swift
@@ -0,0 +1,27 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Foundation
+import Viperit
+
+final class ___FILEBASENAMEASIDENTIFIER___Presenter: Presenter {
+}
+
+
+// MARK: - VIPER COMPONENTS API (Auto-generated code)
+private extension ___FILEBASENAMEASIDENTIFIER___Presenter {
+    var view: ___FILEBASENAMEASIDENTIFIER___ViewInterface {
+        return _view as! ___FILEBASENAMEASIDENTIFIER___ViewInterface
+    }
+    var interactor: ___FILEBASENAMEASIDENTIFIER___Interactor {
+        return _interactor as! ___FILEBASENAMEASIDENTIFIER___Interactor
+    }
+    var router: ___FILEBASENAMEASIDENTIFIER___Router {
+        return _router as! ___FILEBASENAMEASIDENTIFIER___Router
+    }
+}
---
+++ Xcode Templates/Viperit/Viperit Module.xctemplate/Universal/___FILEBASENAME___Router.swift
@@ -0,0 +1,20 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+import Foundation
+import Viperit
+
+final class ___FILEBASENAMEASIDENTIFIER___Router: Router {
+}
+
+// MARK: - VIPER COMPONENTS API (Auto-generated code)
+private extension ___FILEBASENAMEASIDENTIFIER___Router {
+    var presenter: ___FILEBASENAMEASIDENTIFIER___Presenter {
+        return _presenter as! ___FILEBASENAMEASIDENTIFIER___Presenter
+    }
+}
