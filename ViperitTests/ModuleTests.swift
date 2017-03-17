c07e983a5c9496528d8bdd0bf7fcfc664c00577d
Ferran Abelló <ferran.abello@gmail.com>
2017-03-17 13:42:14 +0100

    Test module builder performance

---
+++ ViperitTests/ModuleTests.swift
@@ -0,0 +1,31 @@
+//
+//  ModuleTests.swift
+//  Viperit
+//
+//  Created by Ferran Abelló on 17/03/2017.
+//  Copyright © 2017 Ferran Abelló. All rights reserved.
+//
+
+import XCTest
+import Viperit
+
+private enum TestModules: String, ViperitModule {
+    case sample
+}
+
+class ModuleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+
+    func testModuleBuilderPerformance() {
+        moduleBuilderPerformance()
+    }
+    
+    private func moduleBuilderPerformance() {
+        self.measure {
+            _ = Module.build(TestModules.sample, bundle: Bundle(for: SampleRouter.self))
+        }
+    }
+}
---
+++ ViperitTests/Sample/SamplePresenter.swift
@@ -0,0 +1,27 @@
+//
+//  SamplePresenter.swift
+//  Viperit
+//
+//  Created by Ferran Abelló on 17/03/2017.
+//  Copyright © 2017 Ferran Abelló. All rights reserved.
+//
+
+import Foundation
+import Viperit
+
+final class SamplePresenter: Presenter {
+}
+
+
+// MARK: - VIPER COMPONENTS API (Auto-generated code)
+private extension SamplePresenter {
+    var view: SampleViewInterface {
+        return _view as! SampleViewInterface
+    }
+    var interactor: SampleInteractor {
+        return _interactor as! SampleInteractor
+    }
+    var router: SampleRouter {
+        return _router as! SampleRouter
+    }
+}
