f348c661b65a8057b31a9553f5b034ca2a494e39
Ferran Abelló <ferran.abello@gmail.com>
2017-03-18 14:12:16 +0100

    Module build components test

--- ViperitTests/ModuleTests.swift
+++ ViperitTests/ModuleTests.swift
@@ -16,7 +16,17 @@ private enum TestModules: String, ViperitModule {
 class ModuleTests: XCTestCase {
     
     func testModuleBuildCorrectComponents() {
-    
+        let module = Module.build(TestModules.sample, bundle: Bundle(for: SampleRouter.self))
+        
+        if  let v = module.view as? SampleView,
+            let i = module.interactor as? SampleInteractor,
+            let p = module.presenter as? SamplePresenter,
+            let r = module.router as? SampleRouter,
+            let d = module.displayData as? SampleDisplayData {
+            XCTAssert(true)
+        } else {
+            XCTAssert(false)
+        }
     }
 
     func testModuleBuilderPerformance() {
