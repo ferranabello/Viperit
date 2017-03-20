b0bf00266b9cbf2046ed146d0319cb2ff3763d02
Ferran Abelló <ferran.abello@gmail.com>
2017-03-20 13:49:53 +0100

    Test router embedInNavigationController

---
+++ ViperitTests/Sample/TestModules.swift
@@ -0,0 +1,13 @@
+//
+//  TestModules.swift
+//  Viperit
+//
+//  Created by Ferran Abelló on 20/03/2017.
+//  Copyright © 2017 Ferran Abelló. All rights reserved.
+//
+
+import Viperit
+
+enum TestModules: String, ViperitModule {
+    case sample
+}
