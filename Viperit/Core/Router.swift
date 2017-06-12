d7503e9db9541902de9a3b8fe3511e501660548f
Ferran Abelló <ferran.abello@gmail.com>
2017-06-12 12:26:19 +0200

    Remove similar code for stretchToBounds function

--- Viperit/Core/Router.swift
+++ Viperit/Core/Router.swift
@@ -84,14 +84,18 @@ public extension Router {
     
     private func stretchToBounds(_ holderView: UIView, view: UIView) {
         view.translatesAutoresizingMaskIntoConstraints = false
-        let pinTop = NSLayoutConstraint(item: view, attribute: .top, relatedBy: .equal,
-                                        toItem: holderView, attribute: .top, multiplier: 1.0, constant: 0)
-        let pinBottom = NSLayoutConstraint(item: view, attribute: .bottom, relatedBy: .equal,
-                                           toItem: holderView, attribute: .bottom, multiplier: 1.0, constant: 0)
-        let pinLeft = NSLayoutConstraint(item: view, attribute: .left, relatedBy: .equal,
-                                         toItem: holderView, attribute: .left, multiplier: 1.0, constant: 0)
-        let pinRight = NSLayoutConstraint(item: view, attribute: .right, relatedBy: .equal,
-                                          toItem: holderView, attribute: .right, multiplier: 1.0, constant: 0)
-        holderView.addConstraints([pinTop, pinBottom, pinLeft, pinRight])
+        let pinDirections: [NSLayoutAttribute] = [.top, .bottom, .left, .right]
+        let pinConstraints = pinDirections.map { direction -> NSLayoutConstraint in
+            return NSLayoutConstraint(
+                item: view,
+                attribute: direction,
+                relatedBy: .equal,
+                toItem: holderView,
+                attribute: direction,
+                multiplier: 1.0,
+                constant: 0
+            )
+        }
+        holderView.addConstraints(pinConstraints)
     }
 }
