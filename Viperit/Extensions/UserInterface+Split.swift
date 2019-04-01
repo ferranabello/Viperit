1b252f6fdcc67c6d28652f6a9dfc62957c4119d7
Ferran <ferran.abello@gmail.com>
2019-04-01 16:32:47 +0200

    Change UserInterface file structure

--- Viperit/Extensions/UserInterface+Split.swift
+++ Viperit/Extensions/UserInterface+Split.swift
@@ -1,32 +1,15 @@
 //
-//  UserInterface.swift
+//  UserInterface+Split.swift
 //  Viperit
 //
-//  Created by Ferran on 11/09/2016.
-//  Copyright © 2016 Ferran Abelló. All rights reserved.
+//  Created by Ferran on 01/04/2019.
+//  Copyright © 2019 Ferran Abelló. All rights reserved.
 //
 
 import UIKit
 
-public protocol UserInterfaceProtocol: class, ViperitComponent {
-    var _presenter: PresenterProtocol! { get set }
-    var _displayData: DisplayData! { get set }
-    var viewController: UIViewController { get }
-    func viewDidLoad()
-    func viewWillAppear(_ animated: Bool)
-    func viewDidAppear(_ animated: Bool)
-    func viewWillDisappear(_ animated: Bool)
-    func viewDidDisappear(_ animated: Bool)
-}
-
-public extension UserInterfaceProtocol {
-    var viewController: UIViewController {
-        return self as! UIViewController
-    }
-}
-
-//MARK: - Default implementation for UIViewController
-open class UserInterface: UIViewController, UserInterfaceProtocol {
+//MARK: - Default implementation for UISplitViewController
+open class SplitUserInterface: UISplitViewController, UserInterfaceProtocol {
     public var _presenter: PresenterProtocol!
     public var _displayData: DisplayData!
     
